17572a305edda2388ee9d5197ff5f0135fd15c4a
Uka36 <sethnigus6@yahoo.com>
2022-12-10 00:02:31 -0600

    Player Character Dash
    - added dash
    - refined movement controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,65 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject activePlayer;
 
+    [Header ("Movement Variables")]
+    [SerializeField] float speed;
+    [SerializeField] float dashDis;
+    [SerializeField] float dashTime;
+
+    bool dash = false;
+
     Vector3 moveVector;
-    float speed = 5f;
+
+    //Dash
+    float dashTimer;
+    float dashStartX;
+    float dashStartY;
+    float dashX;
+    float dashY;
+    Vector3 dashEnd;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))   {moveVector.x += 1f;}
-        if (Input.GetKeyDown(KeyCode.LeftArrow))    {moveVector.x -= 1f;}
-        if (Input.GetKeyDown(KeyCode.UpArrow))      {moveVector.y += 1f;}
-        if (Input.GetKeyDown(KeyCode.DownArrow))    {moveVector.y -= 1f;}
+        //Moving around
+        if (!dash)
+        {
+            //Set move vector
+            moveVector.x = 0; moveVector.y = 0;
+            if (Input.GetKey(KeyCode.RightArrow))   {moveVector.x = 1f;}
+            if (Input.GetKey(KeyCode.LeftArrow))    {moveVector.x = -1f;}
+            if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow)) {moveVector.x = 0;}
+            if (Input.GetKey(KeyCode.UpArrow))      {moveVector.y = 1f;}
+            if (Input.GetKey(KeyCode.DownArrow))    {moveVector.y = -1f;}
+            if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow))    {moveVector.y = 0;}
 
-        if (Input.GetKeyUp(KeyCode.RightArrow))   {moveVector.x -= 1f;}
-        if (Input.GetKeyUp(KeyCode.LeftArrow))    {moveVector.x += 1f;}
-        if (Input.GetKeyUp(KeyCode.UpArrow))      {moveVector.y -= 1f;}
-        if (Input.GetKeyUp(KeyCode.DownArrow))    {moveVector.y += 1f;}
+            //Move active player
+            activePlayer.transform.Translate(moveVector.normalized * speed * Time.deltaTime);
+        }
+        
+        //Dash input
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            dash = true;
+            
+            dashStartX = activePlayer.transform.position.x;
+            dashEnd.x = dashStartX + (moveVector.normalized.x * dashDis);
+            dashStartY = activePlayer.transform.position.y;
+            dashEnd.y = dashStartY + (moveVector.normalized.y * dashDis);
+            dashTimer = 0;
+        }
 
-        activePlayer.transform.Translate(moveVector.normalized * speed * Time.deltaTime);
+        //Dashing
+        if (dash)
+        {
+            if (dashTimer < dashTime)
+            {
+                dashX = Mathf.Lerp(dashStartX, dashEnd.x, dashTimer/dashTime);
+                dashY = Mathf.Lerp(dashStartY, dashEnd.y, dashTimer/dashTime);
+                dashTimer += Time.deltaTime;
+
+                activePlayer.transform.position = new Vector3(dashX, dashY, 0);
+            }
+            else {dash = false;}
+        }
     }
 }
