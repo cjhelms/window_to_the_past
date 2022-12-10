f2ac564b6024d2d8a3f7020ea3c2c4da41da3b80
Uka36 <sethnigus6@yahoo.com>
2022-12-10 10:18:39 -0600

    Adjusted Player Movement
    - added slight acceleration/deceleration

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,45 +7,88 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject activePlayer;
 
     [Header ("Movement Variables")]
-    [SerializeField] float speed;
-    [SerializeField] float dashDis;
-    [SerializeField] float dashTime;
-
-    bool dash = false;
-
+    //Movement
     Vector3 moveVector;
+    bool rightInput;
+    bool leftInput;
+    bool upInput;
+    bool downInput;
+    int xVelocity;
+    int yVelocity;
+    Vector3 velocityVector;
+    float accTimer;
+    [SerializeField] float acceleration;
+    [SerializeField] float speed;
 
     //Dash
+    bool dash = false;
     float dashTimer;
     float dashStartX;
     float dashStartY;
     float dashX;
     float dashY;
     Vector3 dashEnd;
+    [SerializeField] float dashDis;
+    [SerializeField] float dashTime;
 
     void Update()
     {
         //Moving around
         if (!dash)
         {
-            //Set move vector
-            moveVector.x = 0; moveVector.y = 0;
-            if (Input.GetKey(KeyCode.RightArrow))   {moveVector.x = 1f;}
-            if (Input.GetKey(KeyCode.LeftArrow))    {moveVector.x = -1f;}
-            if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow)) {moveVector.x = 0;}
-            if (Input.GetKey(KeyCode.UpArrow))      {moveVector.y = 1f;}
-            if (Input.GetKey(KeyCode.DownArrow))    {moveVector.y = -1f;}
-            if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow))    {moveVector.y = 0;}
+            //Listen for inputs
+            if (Input.GetKey(KeyCode.RightArrow)) {rightInput = true;} else {rightInput = false;}
+            if (Input.GetKey(KeyCode.LeftArrow))  {leftInput = true;}  else {leftInput = false;}
+            if (Input.GetKey(KeyCode.UpArrow))    {upInput = true;}    else {upInput = false;}
+            if (Input.GetKey(KeyCode.DownArrow))  {downInput = true;}  else {downInput = false;}
+
+            //Set velocity targets
+            if (rightInput) {xVelocity = 1;}
+            if (leftInput)  {xVelocity = -1;}
+            if ((rightInput && leftInput) || (!rightInput && !leftInput)) {xVelocity = 0;}
+            if (upInput)    {yVelocity = 1;}
+            if (downInput)  {yVelocity = -1;}
+            if ((upInput && downInput) || (!upInput && !downInput))       {yVelocity = 0;}
+            //Create vector and normalize
+            velocityVector = new Vector3(xVelocity, yVelocity, 0).normalized;
+
+            //Adjust velocity x
+            if (moveVector.x < velocityVector.x) 
+            {
+                moveVector.x += acceleration;
+                if (moveVector.x > 1) {moveVector.x = 1;}
+            }
+            if (moveVector.x > velocityVector.x)
+            {
+                moveVector.x -= acceleration;
+                if (moveVector.x < -1) {moveVector.x = -1;}
+            }
+
+            //Adjust velocity y
+            if (moveVector.y < velocityVector.y) 
+            {
+                moveVector.y += acceleration;
+                if (moveVector.y > 1) {moveVector.y = 1;}
+            }
+            if (moveVector.y > velocityVector.y)
+            {
+                moveVector.y -= acceleration;
+                if (moveVector.y < -1) {moveVector.y = -1;}
+            }
+
+            //Round near zero
+            if (Mathf.Abs(moveVector.x) < acceleration) {moveVector.x = 0;}
+            if (Mathf.Abs(moveVector.y) < acceleration) {moveVector.y = 0;}
 
             //Move active player
-            activePlayer.transform.Translate(moveVector.normalized * speed * Time.deltaTime);
+            activePlayer.transform.Translate(moveVector * speed * Time.deltaTime);
         }
         
         //Dash input
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
             dash = true;
-            
+
             dashStartX = activePlayer.transform.position.x;
             dashEnd.x = dashStartX + (moveVector.normalized.x * dashDis);
             dashStartY = activePlayer.transform.position.y;
