5233038d6a98c3f0a565af98123ff828879cb77b
Chris <cjhelms1428@gmail.com>
2022-12-10 11:08:47 -0600

    Add cameras programatically

--- Assets/Scripts/Tiler.cs
+++ Assets/Scripts/Tiler.cs
@@ -4,53 +4,35 @@ using UnityEngine;
 
 public class Tiler : MonoBehaviour
 {
-    private const int MaxDepth = 3;
+    private const int MaxXY = 1;
+    private const int MaxLayerMask = 31;
+    private const int MaxTiles = 12;
+    private const int MaxDepth = MaxTiles - 1;
     private const int MinDepth = 0;
 
-    private Camera cameraMain;
-    private Camera cameraTile1;
-    private Camera cameraTile2;
-    private Camera cameraTile3;
-    private GameObject tileMain;
-    private GameObject tile1;
-    private GameObject tile2;
-    private GameObject tile3;
+    private GameObject cameraMain;
+    private GameObject[] cameras = new GameObject[MaxTiles];
     private int depth;
 
     void Start()
     {
-        cameraMain = GameObject.Find("CameraMain").GetComponent<Camera>();
-        cameraTile1 = GameObject.Find("CameraTile1").GetComponent<Camera>();
-        cameraTile2 = GameObject.Find("CameraTile2").GetComponent<Camera>();
-        cameraTile3 = GameObject.Find("CameraTile3").GetComponent<Camera>();
-        
-        tileMain = GameObject.Find("TileMain");
-        tile1 = GameObject.Find("Tile1");
-        tile2 = GameObject.Find("Tile2");
-        tile3 = GameObject.Find("Tile3");
-        
+        cameraMain = GameObject.Find("CameraMain");
         depth = 0;
 
-        cameraMain.enabled = true;
-        cameraTile1.enabled = false;
-        cameraTile2.enabled = false;
-        cameraTile3.enabled = false;
+        for(int i = 0; i < MaxTiles; i++)
+        {
+            cameras[i] = new GameObject("cam" + i);
+            cameras[i].transform.parent = cameraMain.transform;
+            cameras[i].transform.position = cameraMain.transform.position;
+            Camera cam = cameras[i].AddComponent<Camera>() as Camera;
+            cam.orthographic = true;
+            cam.cullingMask = 1 << GetLayerMask(i) | 1;
+            cam.enabled = false;
+            cam.backgroundColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f),
+                Random.Range(0.0f, 1.0f), 1);
+        }
 
-        // tileMain.setLayer("TileMain");
-        // tile1.setLayer("Tile1");
-        // tile2.setLayer("Tile2");
-        // tile3.setLayer("Tile3");
-
-        tileMain.layer = LayerMask.NameToLayer("TileMain");
-        tile1.layer = LayerMask.NameToLayer("Tile1");
-        tile2.layer = LayerMask.NameToLayer("Tile2");
-        tile3.layer = LayerMask.NameToLayer("Tile3");
-        
-        // Only show the default and corresponding tile layers for each camera
-        cameraMain.cullingMask = 1 << LayerMask.NameToLayer("TileMain");
-        cameraTile1.cullingMask = 1 << LayerMask.NameToLayer("Tile1");
-        cameraTile2.cullingMask = 1 << LayerMask.NameToLayer("Tile2");
-        cameraTile3.cullingMask = 1 << LayerMask.NameToLayer("Tile3");
+        cameras[0].GetComponent<Camera>().enabled = true;
     }
 
     void Flashback()
@@ -85,8 +67,7 @@ public class Tiler : MonoBehaviour
         if(depth < MaxDepth)
         {
             depth++;
-            UpdateCameras();
-            UpdateTiles();
+            AddCamera();
             Debug.Log("Increased depth, new depth: " + depth);
         }
         else
@@ -100,8 +81,7 @@ public class Tiler : MonoBehaviour
         if(depth > MinDepth)
         {
             depth--;
-            UpdateCameras();
-            UpdateTiles();
+            RemoveCamera();
             Debug.Log("Decreased depth, new depth: " + depth);
         }
         else
@@ -110,13 +90,49 @@ public class Tiler : MonoBehaviour
         }
     }
 
-    private void UpdateCameras()
+    private void AddCamera()
     {
-        return;
+        Camera cam = cameras[depth].GetComponent<Camera>();
+        Camera prevCam = cameras[depth - 1].GetComponent<Camera>();
+        if(depth % 2 == 1)
+        {
+            prevCam.rect = new Rect(prevCam.rect.x, prevCam.rect.y, prevCam.rect.width / 2, 
+                prevCam.rect.height);
+            cam.rect = new Rect(prevCam.rect.x + (MaxXY - prevCam.rect.x) / 2, prevCam.rect.y,
+                prevCam.rect.width, prevCam.rect.height);
+            Debug.Log("Halving prevCam width, moving cam right");
+        }
+        else
+        {
+            prevCam.rect = new Rect(prevCam.rect.x, prevCam.rect.y, prevCam.rect.width, 
+                prevCam.rect.height / 2);
+            cam.rect = new Rect(prevCam.rect.x, prevCam.rect.y + (MaxXY - prevCam.rect.y) / 2,
+                prevCam.rect.width, prevCam.rect.height);
+            Debug.Log("Halving prevCam height, moving cam up");
+        }
+        cam.enabled = true;
+        Debug.Log("prevCam: " + prevCam.rect.ToString() + ", cam: " + cam.rect.ToString());
     }
 
-    private void UpdateTiles()
+    private void RemoveCamera()
     {
-        return;
+        Camera cam = cameras[depth].GetComponent<Camera>();
+        cameras[depth + 1].GetComponent<Camera>().enabled = false;
+        if(depth % 2 == 1)
+        {
+            cam.rect = new Rect(cam.rect.x, cam.rect.y, cam.rect.width, cam.rect.height * 2);
+            Debug.Log("Doubling cam height");
+        }
+        else
+        {
+            cam.rect = new Rect(cam.rect.x, cam.rect.y, cam.rect.width * 2, cam.rect.height);
+            Debug.Log("Doubling cam width");
+        }
+        Debug.Log("cam: " + cam.rect.ToString());
+    }
+
+    private LayerMask GetLayerMask(int ndx)
+    {
+        return (LayerMask)(MaxLayerMask - ndx);
     }
 }
